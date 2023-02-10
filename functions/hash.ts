f2db161875fd3764e3c4c492b5295b3661a63b21
BoundlessCarrot <jordan.streete@gmail.com>
2023-02-10 15:33:16 +0100

    fixed func and added mutation

--- functions/hash.ts
+++ functions/hash.ts
@@ -3,8 +3,10 @@ import crypto from 'crypto'
 import graphql from 'graphql'
 
 export default (req: Request, res: Response) => {
-    const url = req.body
+    const url = Buffer.from(JSON.stringify(req.body));
     const hash = crypto.createHash('sha1').update(url).digest('hex');
+
+    // graphql stuff here maybe?
 
     res.status(200).send(`jstr.dev/${hash}`)
 }
