3a2a6b5158d95483a494c2f45ad33167bab12fb0
BoundlessCarrot <jordan.streete@gmail.com>
2023-02-12 20:57:31 +0100

    .

--- functions/hash.ts
+++ functions/hash.ts
@@ -2,13 +2,14 @@ import { Request, Response } from 'express'
 import crypto from 'crypto'
 import graphql from 'graphql'
 
-export default (req: Request, res: Response) => {
+async function insertEntry() {  
+  export default (req: Request, res: Response) => {
     const url = Buffer.from(JSON.stringify(req.body));
     const hash = crypto.createHash('sha1').update(url).digest('hex');
 
     // graphql stuff here maybe?
 
-    const response = fetch("https://skgjjokqnhwykfrwrlcn.hasura.eu-central-1.nhost.run/v1/graphql", {
+    const response = await fetch("https://skgjjokqnhwykfrwrlcn.hasura.eu-central-1.nhost.run/v1/graphql", {
       method: 'POST',
       body: JSON.stringify({
         mutation insertEntries($hash: String!, $url: String!) {
@@ -26,6 +27,7 @@ export default (req: Request, res: Response) => {
     });
 
     res.status(200).send(`jstr.dev/${hash}`);
+  }
 }
 
-
+insertEntry();
